e97b4e4246dad77b047d95a8bacc23086a5ca56d
Asaf Günay <asaf.gunay@windowslive.com>
2019-08-04 12:30:33 +0300

    product/index created

---
+++ StartMVC/StartMVC/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StartMVC.Models;
+
+namespace StartMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Product> productList = Product.GetSampleData();
+            return View(productList);
+        }
+    }
+}
