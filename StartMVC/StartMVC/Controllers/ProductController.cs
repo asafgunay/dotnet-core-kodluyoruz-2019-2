ba3310907cfe244c357fcacbac1979dc0512c7ee
Asaf Günay <asaf.gunay@windowslive.com>
2019-08-04 13:53:32 +0300

    product create method completed

--- StartMVC/StartMVC/Controllers/ProductController.cs
+++ StartMVC/StartMVC/Controllers/ProductController.cs
@@ -9,18 +9,46 @@ namespace StartMVC.Controllers
 {
     public class ProductController : Controller
     {
+        public static List<Product> products;
         public IActionResult Index()
         {
             List<Product> productList = Product.GetSampleData();
+            if(products!=null && products.Any())
+            {
+                productList.AddRange(products);
+            }
             return View(productList);
         }
         public IActionResult Detail(int id)
         {
             List<Product> productList = Product.GetSampleData();
+            if (products != null && products.Any())
+            {
+                productList.AddRange(products);
+            }
             // ornek data icerisindeki parametreden gelen id'ye sahip urunu getir!
             // Where kosulu tum dataList icerisindeki itemlarin kosullandirilmasidir.
             Product item = productList.Where(x => x.Id == id).FirstOrDefault();
             return View(item);
+        }
+        // urun kayit formunu getirecek
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (products == null)
+                {
+                    products = new List<Product>();
+                }
+                products.Add(model);
+                return RedirectToAction("Detail", new { id = model.Id });
+            }
+            return View(model);
         }
     }
 }
--- StartMVC/StartMVC/Models/Product.cs
+++ StartMVC/StartMVC/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,10 @@ namespace StartMVC.Models
     public class Product
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public double Price { get; set; }
+        [Required]
         public string ImageUrl { get; set; }
 
         public static List<Product> GetSampleData()
