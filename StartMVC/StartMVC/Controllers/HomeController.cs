dcdc38c024c5153adf077a47a0b2edb557097c5f
Asaf Günay <asaf.gunay@windowslive.com>
2019-08-04 12:11:09 +0300

    mvc startup template begin

---
+++ StartMVC/StartMVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StartMVC.Models;
+
+namespace StartMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StartMVC/StartMVC/Models/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartMVC.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+
+        public static List<Product> GetSampleData()
+        {
+            var urun1 = new Product
+            {
+                Id = 1,
+                ImageUrl = "https://productimages.hepsiburada.net/s/27/280-413/10182153306162.jpg",
+                Name = "3'lü Bistro Set Balkon Bahçe Mobilya Takımı Masif Akasya Ağacı 2 Sandalye 1 Masa",
+                Price = 399.90
+            };
+            var urun2 = new Product
+            {
+                Id = 2,
+                ImageUrl = "https://productimages.hepsiburada.net/s/26/280-413/10147813195826.jpg",
+                Name = "Sallanır 2 Pozisyonlu Şezlong",
+                Price = 213.12
+            };
+            var urun3 = new Product
+            {
+                Id = 3,
+                ImageUrl = "https://productimages.hepsiburada.net/s/1/280-413/9529689636914.jpg",
+                Name = "Düz Krem Plaj Şemsiyesi",
+                Price = 210.14
+            };
+            return new List<Product>();
+        }
+    }
+}
