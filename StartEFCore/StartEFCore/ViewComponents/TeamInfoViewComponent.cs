7bd572468caa7b9e63616ccd5f85711f094ea7fb
Asaf Günay <asaf.gunay@windowslive.com>
2019-08-24 13:37:26 +0300

    Update

---
+++ StartEFCore/StartEFCore/ViewComponents/TeamInfoViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StartEFCore.ViewComponents
+{
+    public class TeamInfoViewComponent
+    {
+
+    }
+}
