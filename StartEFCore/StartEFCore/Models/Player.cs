25ae8f9bf841e0dba1fe022ab8e35757f26dcf20
Asaf Günay <asaf.gunay@windowslive.com>
2019-08-25 11:19:14 +0300

    DataValidation completed

--- StartEFCore/StartEFCore/Models/Player.cs
+++ StartEFCore/StartEFCore/Models/Player.cs
@@ -10,23 +10,27 @@ namespace StartEFCore.Models
 {
     public class Player : Entity
     {
-        [Required]
-        [Display(Name ="Ad Soyad")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
+        [StringLength(30, ErrorMessage = "{0} alanı geçerli maksimum karakter uzunluğu {1}, minimum karakter uzunluğu ise {2}", MinimumLength = 5)]
+        [Display(Name = "Ad Soyad")]
         public string LongName { get; set; }
-        [Display(Name ="Yaş")]
+        [Display(Name = "Yaş")]
+        [Range(17, 44, ErrorMessage = "{0} sınırı {1} - {2} arasındadır")]
         public int? Age { get; set; }
-        [Required]
-        [Display(Name ="Numara")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
+        [Display(Name = "Numara")]
+        [Range(1, 99, ErrorMessage = "{0} seçim aralığı {1} - {2} arasındadır")]
         public int Number { get; set; }
-        [Required]
-        [Display(Name ="Pozisyon")]
+        [Required(ErrorMessage = "{0} alanı zorunludur.")]
+        [Display(Name = "Pozisyon")]
+        [StringLength(30, ErrorMessage = "{0} alanı içib maksimum karakter uzunluğu {1}")]
         public string Position { get; set; }
-        [Display(Name ="Resim")]
+        [Display(Name = "Resim")]
         public string ImageUrl { get; set; }
-        [Display(Name ="Takım")]
+        [Display(Name = "Takım")]
         public virtual int? TeamId { get; set; }
         [ForeignKey("TeamId")]
-        [Display(Name ="Takım")]
+        [Display(Name = "Takım")]
         public virtual Team Team { get; set; }
 
     }
--- StartEFCore/StartEFCore/Models/Team.cs
+++ StartEFCore/StartEFCore/Models/Team.cs
@@ -10,17 +10,19 @@ namespace StartEFCore.Models
     public class Team : Entity<int>
     {
 
-        [Required]
-        [Display(Name ="Takım Adı")]
+        [Required(ErrorMessage = "Takım adı alanı zorunludur")]
+        [Display(Name = "Takım Adı")]
+        [StringLength(25, ErrorMessage = "{0} maksimum {1} karakter, minimum {2} karakter uzunluğunda olmalı", MinimumLength = 5)]
         public string Name { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Logo alanı zorunludur")]
         [DataType(DataType.ImageUrl)]
-        [Display(Name ="Logo")]
+        [Display(Name = "Logo")]
         public string LogoUrl { get; set; }
-        [Required]
-        [Display(Name ="Şehir")]
+        [Required(ErrorMessage = "Şehir alanı zorunludur")]
+        [Display(Name = "Şehir")]
         public string Province { get; set; }
-        [Display(Name ="Kuruluş")]
+        [Display(Name = "Kuruluş")]
+        [Range(1870, 2019, ConvertValueInInvariantCulture = true, ErrorMessage = "{0} yıl aralığı {1} - {2} olmalı")]
         public int? Year { get; set; }
 
         public virtual ICollection<Player> Players { get; set; }
