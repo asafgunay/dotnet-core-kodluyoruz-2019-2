79f786284e508228246f344a1c499f8d92da82d7
Asaf Günay <asaf.gunay@windowslive.com>
2019-09-07 11:22:45 +0300

    Ef migration and database comments

--- DotNetCoreIdentity/src/DotNetCoreIdentity.EF/Context/ApplicationUserDbContext.cs
+++ DotNetCoreIdentity/src/DotNetCoreIdentity.EF/Context/ApplicationUserDbContext.cs
@@ -13,7 +13,17 @@ namespace DotNetCoreIdentity.EF.Context
         {
 
         }
+        // veritabani degisikligi icin migration almaliyiz
+        // Package Manager Console icin migration almak(Context'i olusturdugunuz proje default project alaninda secili olmali)
+        // Add-Migration MigrationAdi
+        // CLI icin(context'i olusturdugunuz projenin dizininde olmalisiniz)
+        // dotnet ef migrations add MigrationAdi
 
+        // olusturulan migration ile veritabanina yansitilacak degisimlerin kodlari uretilir. Bu kodlarin veritabaninda calismasi icin asagidaki komutu calistirin
+        // Package Manager Console icin migration almak(Context'i olusturdugunuz proje default project alaninda secili olmali)
+        // Update-Database
+        // CLI icin(context'i olusturdugunuz projenin dizininde olmalisiniz)
+        // dotnet ef database update
         /*
          DBSet'ler buraya
          */
