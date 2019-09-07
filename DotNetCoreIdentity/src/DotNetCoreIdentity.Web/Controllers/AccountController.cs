ae4f82a90b5b08fd039951453bb1461524fb1aef
Asaf Günay <asaf.gunay@windowslive.com>
2019-09-07 12:48:53 +0300

    accountcontroller user services imp..

--- DotNetCoreIdentity/src/DotNetCoreIdentity.Web/Controllers/AccountController.cs
+++ DotNetCoreIdentity/src/DotNetCoreIdentity.Web/Controllers/AccountController.cs
@@ -2,13 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetCoreIdentity.Domain.Identity;
 using DotNetCoreIdentity.Web.ViewModels.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNetCoreIdentity.Web.Controllers
 {
     public class AccountController : Controller
     {
+        // Kullanici kaydetmek icin veya kullanici bilgilerinde degisiklik yapmak icin kullanilan servis.
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        // Kullanicinin uygulamaya giris cikis islemlerini yonettigimiz servis.
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Login()
         {
             return View();
@@ -23,9 +38,28 @@ namespace DotNetCoreIdentity.Web.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Register(RegisterViewModel model)
+        public async Task<IActionResult> Register(RegisterViewModel model)
         {
-            return View();
+            // gelen modeli valide et
+            if (ModelState.IsValid)
+            {
+                // validse kaydet
+
+                // kaydetme basarisizsa
+                // hatalari modelstate e ekle
+                // AddErrors()
+
+            }
+            // degilse hatalari don
+            return View(model);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var err in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, err.Description);
+            }
         }
     }
 }
