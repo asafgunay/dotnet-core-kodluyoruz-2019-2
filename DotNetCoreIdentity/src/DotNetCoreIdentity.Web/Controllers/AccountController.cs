d421ff7a0cf6d1715dd55d473daa6fcf8bb56b4b
Asaf Günay <asaf.gunay@windowslive.com>
2019-09-07 10:58:45 +0300

    account controller created

---
+++ DotNetCoreIdentity/src/DotNetCoreIdentity.Web/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreIdentity.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ DotNetCoreIdentity/src/DotNetCoreIdentity.Web/ViewModels/Identity/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreIdentity.Web.ViewModels.Identity
+{
+    public class LoginViewModel
+    {
+    }
+}
