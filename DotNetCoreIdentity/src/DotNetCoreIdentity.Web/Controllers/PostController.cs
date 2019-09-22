de28a9bdd744e073136bc4fc29dd1bdd84285f8c
Asaf Günay <asaf.gunay@windowslive.com>
2019-09-22 13:03:11 +0300

    Create and Index view created

--- DotNetCoreIdentity/src/DotNetCoreIdentity.Web/Controllers/PostController.cs
+++ DotNetCoreIdentity/src/DotNetCoreIdentity.Web/Controllers/PostController.cs
@@ -2,27 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetCoreIdentity.Application;
 using DotNetCoreIdentity.Application.BlogServices;
+using DotNetCoreIdentity.Application.BlogServices.Dtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DotNetCoreIdentity.Web.Controllers
 {
     public class PostController : Controller
     {
         private readonly IPostService _postService;
-        public PostController(IPostService postService)
+        private readonly ICategoryService _categoryService;
+
+        public PostController(IPostService postService, ICategoryService categoryService)
         {
             _postService = postService;
+            _categoryService = categoryService;
         }
         // liste
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var getAllService = await _postService.GetAll();
+            List<PostDto> model = getAllService.Result;
+            return View(model);
         }
         public async Task<IActionResult> Create()
         {
-            return View();
+            var categoryList = await _categoryService.GetAll();
+            ViewBag.CategoryDDL = categoryList.Result.Select(x => new SelectListItem
+            {
+                Selected= false,
+                Text=x.Name,
+                Value = x.Id.ToString()
+            }).ToList();
 
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreatePostInput model)
+        {
+            return View(model);
         }
     }
 }
